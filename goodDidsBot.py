d0795ecdcd7c125d65a687e147730a0110b6f0eb
Sergio Keler <skeler@opendesign.com>
2019-09-04 16:18:24 +0300

    dialog

--- goodDidsBot.py
+++ goodDidsBot.py
@@ -4,22 +4,36 @@
 import telebot
 from telebot import types
 import logging
+import random
 import botConfig
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def loadTextFile(textFileName):
+    r=dict();
+    try:
+        r['status']='ok'
+        r['text']=[line.rstrip('\n') for line in open(textFileName)]
+    except Exception as e:
+        r={'status':'error', 'text':str(e)}
+    return r
+
+def getHelp():
+    return loadTextFile(botConfig.helpTextFile)
+
+def getCongrats():
+    return loadTextFile(botConfig.congratsTextFile)
 
 bot=telebot.TeleBot(botConfig.token)
 
 @bot.message_handler(commands=['start','help'])
 def reply_help(message):
-    #        reply_to(message,"Это подсказка" + repr(message.from_user.__dir__()))
-    #with message.from_user:
-        bot.reply_to(message,"First '{}' Second '{}' User '{}' is-bot '{}' lang '{}'".format(message.from_user.first_name,message.from_user.last_name,message.from_user.username,message.from_user.is_bot,message.from_user.language_code))
-
-@bot.message_handler(commands=['show'])
-def reply_show(message):
-    try:
-        bot.reply_to(message,"{} = {}".format(message.text,eval( "repr({})".format( (message.text.split(' '))[1] ))))
-    except Exception as e:
-        bot.reply_to(message,"Error listing object: " + str(e))
+    helpText=getHelp()
+    if helpText['status']=='error':
+        logging.error("Cannot show help. {}".format(helpText['text']))
+        return
+    bot.reply_to(message,'\n'.join(helpText['text']).format(message.from_user.first_name, message.from_user.last_name, message.from_user.username))
 
 @bot.message_handler(commands=['list'])
 def reply_list(message):
@@ -37,7 +51,11 @@ def reply_list(message):
         bot.reply_to(message,"Error listing object: " + str(e))
 
 @bot.message_handler(func=lambda message: True)
-def reply_echo(message):
-    bot.reply_to(message,"Ты сказал: %s" % message.text)
+def reply_dialog(message):
+    congratsText=getCongrats()
+    if congratsText['status']=='error':
+        logging.error("Cannot show help. {}".format(congratsText['text']))
+        return
+    bot.reply_to(message,random.choice(congratsText['text']))
 
 bot.polling()
