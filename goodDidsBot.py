04288a7e22c54333a8df28d82266e3989cf4edcd
Sergio Keler <skeler@opendesign.com>
2019-10-07 18:05:23 +0300

    buttons

--- goodDidsBot.py
+++ goodDidsBot.py
@@ -9,6 +9,8 @@ import time,sys,signal
 import datetime
 import mysql.connector
 
+from pprint import pprint
+
 import botConfig
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -60,51 +62,69 @@ def reply_help(message):
     if helpText['status']=='error':
         logging.error("Cannot show help. {}".format(helpText['text']))
         return
-    bot.reply_to(message,'\n'.join(helpText['text']).format(message.from_user.first_name, message.from_user.last_name, message.from_user.username))
+    bot.send_message(message.chat.id,'\n'.join(helpText['text']).format(message.from_user.first_name, message.from_user.last_name, message.from_user.username))
 
 @bot.message_handler(commands=['list'])
 def reply_list(message):
     try:
         obj=(message.text.split(' '))[1]
-        bot.reply_to(message,"List of '{}':".format(obj))
+        bot.send_message(message.chat.id,"List of '{}':".format(obj))
         for field in eval("{}.__dir__()".format(obj)):
             typ=eval("str(type({}.{}))".format(obj,field))
             val=eval("repr({}.{})".format(obj,field))
             msg="{}:{} {}\n".format(field,typ,val)
             #if not typ.find("'builtin_function_or_method'") and not typ.find("'method-wrapper'") and not field.startswith('_')
             if not field.startswith('_'):
-                bot.reply_to(message,"list of {}:\n{}".format(obj,msg))
+                bot.send_message(message.chat.id,"list of {}:\n{}".format(obj,msg))
     except Exception as e:
-        bot.reply_to(message,"Error listing object: " + str(e))
+        bot.send_message(message.chat.id,"Error listing object: " + str(e))
+
+def send_dids_list(uid,cid):
+    cursor=myConn.cursor()
+    cursor.execute("select * from did where cid=%s order by dt desc limit 5",(uid,))
+    s='\n'.join([t[2] for t in cursor.fetchall()])
+    bot.send_message(cid,s)
 
 @bot.message_handler(func=lambda message: True)
 def reply_dialog(message):
+    bot.send_message(message.chat.id, "Ваши добрые дела...", reply_markup=gen_buttons(message))
     if (message.text.split(' '))[0].lower() == 'покажи':
-        cursor=myConn.cursor()
-        #logging.info("select * from did where cid=%s",(message.from_user.id,))
-        cursor.execute("select * from did where cid=%s order by dt desc limit 5",(message.from_user.id,))
-        s='\n'.join([t[2] for t in cursor.fetchall()])
-        bot.reply_to(message,s)
+        send_dids_list(message.from_user.id,message.chat.id)
     elif (message.text.split(' '))[0].lower() == 'я':
         congratsText=getCongrats()
         if congratsText['status']=='error':
             logging.error("Cannot show help. {}".format(congratsText['text']))
             return
         doDbRecord(message)
-        bot.reply_to(message,random.choice(congratsText['text']))
+        bot.send_message(message.chat.id,random.choice(congratsText['text']))
     elif (message.text.split(' '))[0].lower() == 'хочу':
         planCongratsText=getPlanCongrats()
         if planCongratsText['status']=='error':
             logging.error("Cannot show help. {}".format(planCongratsText['text']))
             return
         doDbRecord(message)
-        bot.reply_to(message,random.choice(planCongratsText['text']))
+        bot.send_message(message.chat.id,random.choice(planCongratsText['text']))
     else:
         typeErrorsText=getTypeErrors()
         if typeErrorsText['status']=='error':
             logging.error("Cannot show help. {}".format(typeErrorsText['text']))
             return
-        bot.reply_to(message,random.choice(typeErrorsText['text']))
+        bot.send_message(message.chat.id,random.choice(typeErrorsText['text']))
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_button_query(call):
+    if call.data == "buttonList":
+        bot.answer_callback_query(call.id, "Надо показать список")
+        #send_dids_list(uid,cid)
+    elif call.data == "buttonDid":
+        bot.answer_callback_query(call.id, "Надо спросить доброе дело")
+
+def gen_buttons(msg):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    buttonList = types.InlineKeyboardButton('Список',callback_data="buttonList")
+    buttonDid = types.InlineKeyboardButton('Дело',callback_data="buttonDid")
+    markup.add(buttonList,buttonDid)
+    return markup
 
 def interuppt_handler(signum, frame):
     logging.info("Ctrl-C pressed.")
